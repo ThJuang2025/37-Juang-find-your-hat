cc3d6ae5e6333572a1148b2d48737edcfcea8981
THJuang2025 <th.juang.2025@gmail.com>
2025-11-30 15:35:41 +0700

    feat:Add gameRule Fuction and Revise Control flow

--- main.js
+++ main.js
@@ -28,16 +28,43 @@ function printBoard(board) {
 
 printBoard(board);
 
+function gameRule(row, col) {
+  if (row < 0 || row > 2 || col < 0 || col > 2) {
+    console.log("Out of bounds! You lose :(");
+    playing = false;
+  } else if (board[row][col] === HAT) {
+    console.log("*** You WIN ***!");
+    playing = false;
+  } else if (board[row][col] === HOLE) {
+    console.log("Sorry, you fell in a hole :(");
+    playing = false;
+  }
+}
+
 // Keyboard-Entering Key to move PLAYER
 const VALID_KEYS = ['w', 'a', 's', 'd'];
 //  Read Input from user  that input correctly and turn to lowercase for conveneient check
-const input = prompt("Which way? (w/a/s/d): ").toLowerCase();;
 
-while (playing) 
-if (VALID_KEYS.includes(input)) {
-  console.log("✅ Valid input:", input);
-  playing = false;  //to exxist loop
-} else {
-  console.log("❌ Invalid input. Please enter w, a, s, or d to move PLAYER.");
+
+while (playing) {
+	const input = prompt("Which way? (w/a/s/d): ").toLowerCase();
+	if (VALID_KEYS.includes(input)) {
+		console.log("✅ Valid input:", input);
+		 // ลบตำแหน่งเดิม
+    	board[playerRow][playerCol] = EMPTY;
+		// อัปเดตตำแหน่งผู้เล่นตามปุ่มที่กด
+		if (input === "w") playerRow--; // move Up
+		if (input === "s") playerRow++; // move Down
+		if (input === "a") playerCol--; // move Left
+		if (input === "d") playerCol++; // move Right
+		gameRule(playerRow,playerCol);
+		if (playing) {
+			board[playerRow][playerCol] = PLAYER;
+			printBoard(board);
+		}
+	} else {
+	console.log("❌ Invalid input. Please enter w, a, s, or d to move PLAYER.");
+	};
 };
+
 
